3752ea9c63ba6639051f78dbe076012ae96fec8f
Sander <sander@wieni.be>
2020-03-28 14:40:23 +0100

    Update webpack-dev-script

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,65 @@
-const webpack = require("webpack");
-const chalk = require("chalk");
-const { argv } = require("yargs");
-
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const boxen = require("boxen");
+const chalk = require("chalk");
 
-const build = (() => {
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const clearConsole = require("react-dev-utils/clearConsole");
+const openBrowser = require("react-dev-utils/openBrowser");
+
+const { welcomeMessage, resolveConfigFile, setMagicEnv } = require("./utils");
+
+const isInteractive = process.stdout.isTTY;
+
+const magicStart = () => {
+  setMagicEnv("development");
+
   const appDirectory = fs.realpathSync(process.cwd());
   const theme = path.basename(appDirectory);
 
-  
-})();
+  welcomeMessage(theme);
+
+  const magicConfigFilePath = path.resolve(appDirectory, "magic.config.js");
+  const magicConfig = resolveConfigFile(magicConfigFilePath);
+
+  const magicDevConfigFilePath = path.resolve(
+    appDirectory,
+    "magic.dev.config.js"
+  );
+  const magicDevConfig = resolveConfigFile(magicDevConfigFilePath);
+
+  const webpackConfig = require("../webpack.config")(theme);
+  const webpackDevConfig = require("../webpack.dev.config")(
+    theme,
+    magicDevConfig
+  );
+
+  const compiler = webpack({
+    ...webpackConfig,
+    mode: "development",
+  });
+  const devServer = new WebpackDevServer(compiler, webpackDevConfig);
+
+  devServer.listen(magicDevConfig.proxyPort, "localhost", (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    if (isInteractive) {
+      clearConsole();
+    }
+
+    const localServerUrl = `http://localhost:${magicDevConfig.proxyPort}`;
+    openBrowser(localServerUrl);
+    console.log(
+      boxen(
+        `🎩 Loading Magic Development Server:\n${chalk.red(localServerUrl)}`,
+        {
+          padding: 1,
+        }
+      )
+    );
+  });
+};
+
+magicStart();
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,29 +1,26 @@
-const config = require("./webpack.config");
-const drupackConfig = require("../drupack/magic.dev.config");
 const path = require("path");
+const errorOverlayMiddleware = require("react-dev-utils/errorOverlayMiddleware");
 
-module.exports = {
-  ...config,
-  mode: "development",
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    quiet: false,
-    hot: true,
-    overlay: {
-      errors: true,
-    },
-    port: drupackConfig.proxyPort,
-    contentBase: path.resolve(__dirname, "../drupack/resources"),
-    headers: { "Access-Control-Allow-Origin": "*" },
-    proxy: {
-      "*": {
-        secure: false,
-        target: drupackConfig.proxyTarget,
-        changeOrigin: true,
-        onProxyReq(proxyReq) {
-          proxyReq.setHeader("host", drupackConfig.proxyHost);
-        },
+module.exports = (theme, magicConfig) => ({
+  quiet: true,
+  hot: true,
+  overlay: {
+    errors: true,
+  },
+  port: magicConfig.proxyPort,
+  contentBase: path.resolve(__dirname, `../${theme}/resources`),
+  headers: { "Access-Control-Allow-Origin": "*" },
+  proxy: {
+    "*": {
+      secure: false,
+      target: magicConfig.proxyTarget,
+      changeOrigin: true,
+      onProxyReq(proxyReq) {
+        proxyReq.setHeader("host", magicConfig.proxyHost);
       },
     },
   },
-};
+  before(app, server) {
+    app.use(errorOverlayMiddleware());
+  },
+});
