14a76278bd18fb58b42d1083c4da8f32083ef486
Sander <sander@wieni.be>
2020-06-08 11:54:54 +0200

    Add plugin-syntax-class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -88,6 +88,7 @@ module.exports = (theme, magicConfig) => ({
               process.env.NODE_ENV !== "production"
                 ? require.resolve("react-hot-loader/babel")
                 : false,
+              "@babel/plugin-syntax-class-properties",
             ].filter(Boolean),
           },
         },
